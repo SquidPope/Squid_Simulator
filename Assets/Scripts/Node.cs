a8debfd4c2351547819aa761d0ad2f5fe69921f4
SquidPope <30853677+SquidPope@users.noreply.github.com>
2018-01-26 23:39:09 -0500

    Added hasNeuron to Nodes
    Nodes now have Neurons. Nodes check neighboring nodes and "switch" the neuron over.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,13 +6,50 @@ public class Node : MonoBehaviour
     [SerializeField]
     List<Node> connectedNodes;
 
-	void Start()
-    {
-		
-	}
+    private bool hasNeuron;
+    Renderer rend;
 
-	void Update()
+    public bool HasNeuron
     {
-	    
-	}
+        get { return hasNeuron; }
+        set
+        {
+            hasNeuron = value;
+            if (hasNeuron)
+            {
+                rend.material.color = Color.white;
+            }
+            else
+            {
+                rend.material.color = Color.gray;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        HasNeuron = false;
+    }
+
+    private void OnMouseUp()
+    {
+        if (HasNeuron)
+        {
+            //error
+        }
+        else
+        {
+            HasNeuron = true;
+            //light up
+            foreach (Node n in connectedNodes)
+            {
+                if (n.HasNeuron)
+                {
+                    n.HasNeuron = false;
+                    break; //Only take one neuron
+                }
+            }
+        }
+    }
 }
