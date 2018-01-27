839d6dfa0b20e2802379ba9b7c801a3b01e1c620
SquidPope <30853677+SquidPope@users.noreply.github.com>
2018-01-26 22:56:09 -0500

    Added Unity Project
    Added base Squid Simulator Unity project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public enum GameState { Brain, Squid }
+public class GameController : MonoBehaviour
+{
+    GameState state;
+
+    static GameController instance;
+    public static GameController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = GameObject.FindGameObjectWithTag("GameController");
+                instance = go.GetComponent<GameController>();
+            }
+
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        state = GameState.Brain;
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    List<Node> connectedNodes;
+
+	void Start()
+    {
+		
+	}
+
+	void Update()
+    {
+	    
+	}
+}
